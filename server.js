141a52ea148dfc3b68c095871578e7b20558fd66
Kaique da Silva Ivo <kaiqueivo1011@gmail.com>
2023-07-31 21:34:42 -0300

    fixed-url-cloud

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const server = require('./app');
 const mongoose = require('mongoose');
 
 const url_cloud = process.env.DB_URL.replace(
-    "<Username>",
+    "<username>",
     process.env.DB_USERNAME
-).replace('<Password>', process.env.DB_PASSWORD); //Sobrescrever url do banco de dados com o login do admin
+).replace('<password>', process.env.DB_PASSWORD); //Sobrescrever url do banco de dados com o login do admin
 
 mongoose.connect(url_cloud, {
     useUnifiedTopology: true,
